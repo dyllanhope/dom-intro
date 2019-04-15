cbca2333783bf009d733c5af5ccd13982997f338
Dyllan Hope <dyllanjhope@gmail.com>
2019-04-15 09:22:54 +0200

    Final bug fixes

--- js/text-bill.js
+++ js/text-bill.js
@@ -1,35 +1,35 @@
 // get a reference to the textbox where the bill type is to be entered
 var billTypeText = document.querySelector(".billTypeText");
 //get a reference to the add button
-var addBtn = document.querySelector(".addToBillBtn");
+var addBtnTex = document.querySelector(".addToBillBtn");
 //create a variable that will keep track of the total bill
-var callsTotalElem = document.querySelector(".callTotalOne");
-var smsTotalElem = document.querySelector(".smsTotalOne");
-var totalCostElem = document.querySelector(".totalOne");
-var smsTotal = 0;
-var callsTotal = 0;
+var callsTotalElemTex = document.querySelector(".callTotalOne");
+var smsTotalElemTex = document.querySelector(".smsTotalOne");
+var totalCostElemTex = document.querySelector(".totalOne");
+var smsTotalTex = 0;
+var callsTotalTex = 0;
 //add an event listener for when the add button is pressed
-addBtn.addEventListener('click', function(){
+addBtnTex.addEventListener('click', function(){
 //in the event listener check if the value in the bill type textbox is 'sms' or 'call'
 // * add the appropriate value to the running total
 // * add nothing for invalid values that is not 'call' or 'sms'.
     var billTypeEntered = billTypeText.value.trim();
     if (billTypeEntered === "call"){
-        callsTotal += 2.75
+        callsTotalTex += 2.75
     }
     else if (billTypeEntered === "sms"){
-        smsTotal += 0.75;
+        smsTotalTex += 0.75;
     }
     // * display the latest total on the screen
-    callsTotalElem.innerHTML = callsTotal.toFixed(2);
-    smsTotalElem.innerHTML = smsTotal.toFixed(2);
-    var totalCost = callsTotal + smsTotal;
-    totalCostElem.innerHTML = totalCost.toFixed(2);
+    callsTotalElemTex.innerHTML = callsTotalTex.toFixed(2);
+    smsTotalElemTex.innerHTML = smsTotalTex.toFixed(2);
+    var totalCostTex = callsTotalTex + smsTotalTex;
+    totalCostElemTex.innerHTML = totalCostTex.toFixed(2);
 
-    if(totalCost >= 50){
-        totalCostElem.classList.add("danger");
-    } else if(totalCost >= 30){
-        totalCostElem.classList.add("warning");
+    if(totalCostTex >= 50){
+        totalCostElemTex.classList.add("danger");
+    } else if(totalCostTex >= 30){
+        totalCostElemTex.classList.add("warning");
     }
 });
 
